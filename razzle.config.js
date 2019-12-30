fd75ad71713bc0955b4da695549cd3b7bdda4f98
Wbert Adrian Castro Vera <wbertc@gmail.com>
2019-12-29 20:33:15 -0500

    Added heroku support

--- razzle.config.js
+++ razzle.config.js
@@ -1,5 +1,11 @@
 // razzle.config.js
+const razzleHeroku = require('razzle-heroku');
 
 module.exports = {
+  modify(config, { target, dev }, webpack) {
+    config = razzleHeroku(config, {target, dev}, webpack);
+
+    return config;
+  },
   plugins: ['scss'],
 };
